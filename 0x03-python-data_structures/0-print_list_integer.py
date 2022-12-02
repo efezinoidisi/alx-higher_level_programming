c02b7bb89c5b13d6a34047f02c4a0c932a356bb5
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-12-02 09:24:38 -0800

    pyscript: print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    print_list_integer - prints a list
+
+    @my_list: list of integers
+
+    """
+
+    for i in my_list:
+        print("{:d}".format(i))
