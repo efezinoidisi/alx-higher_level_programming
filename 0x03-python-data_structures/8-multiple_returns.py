0313aa0f4b249e80926ce182062151646936bc0f
efezino idisi <enduranceidisi13@gmail.com>
2022-12-04 18:55:26 +0100

    pyscript: returns a tuple with the length of a string and its first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length > 0:
+        new_tuple = (length, sentence[0],)
+    else:
+        new_tuple = (length, "None",)
+    return new_tuple
