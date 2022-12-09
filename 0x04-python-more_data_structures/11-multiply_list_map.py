7a2a435b22756ae2bb92aa2c0aed160e60b7920a
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-12-09 10:27:31 -0800

    multiply by using lambda

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,7 +2,4 @@
 
 
 def multiply_list_map(my_list=[], number=0):
-    new_list = []
-    if my_list:
-        new_list = list(map(lambda x: x * number, my_list))
-    return new_list
+    return list(map(lambda x: x * number, my_list))
