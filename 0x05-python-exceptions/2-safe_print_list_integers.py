695adca34979e5893d3069682069e462a2dde252
efezino <enduranceidisi13@gmailcom>
2022-12-19 18:58:41 +0100

    fix: ignored index error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count = count + 1
-        except IndexError:
-            continue
         except TypeError:
             continue
         except ValueError:
