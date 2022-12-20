4d38799e7407705c42e3e027d51bb96e87644fe4
efezino <enduranceidisi13@gmailcom>
2022-12-20 21:09:27 +0100

    class: created a private instance attribute size for the square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a class called Square.
+"""
+
+
+class Square:
+    """This defines a square
+
+
+    Args:
+        size (int): size of the square
+
+
+    """
+    def __init__(self, size):
+        self.__size = size
