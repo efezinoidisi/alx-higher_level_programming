5e90ba0663bd7ad4df2695d80b4d878c8e468c10
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2023-01-03 14:53:17 -0800

    add task 19

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+ This is the 19-copy_list module.
+
+ This module supplies one function, copy_list().
+
+
+ """
+
+
+def copy_list(lst):
+    """ Creates a copy of a list.
+
+     Args:
+         lst (list): the original list
+
+    Returns:
+         A copy of the original list
+    """
+
+    return lst[:]
