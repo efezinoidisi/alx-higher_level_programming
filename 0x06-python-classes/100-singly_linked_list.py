5facbfce776f627d92c2bb3f85fd608901b46a69
efezino <enduranceidisi13@gmailcom>
2023-01-05 01:38:47 +0100

    fix: indentation

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -13,25 +13,25 @@ class Node:
         self.data = data
         self.next_node = next_node
 
-        @property
-        def data(self):
-            return self.__data
+    @property
+    def data(self):
+        return self.__data
 
-        @data.setter
-        def data(self, value):
-            if not isinstance(value, int):
-                raise TypeError("data must be an integer")
-            self.__data = value
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
 
-        @property
-        def next_node(self):
-            return self.__next_node
+    @property
+    def next_node(self):
+        return self.__next_node
 
-        @next_node.setter
-        def next_node(self, value):
-            if value and not isinstance(value, Node):
-                raise TypeError("next_node must be a Node object")
-            self.__next_node = value
+    @next_node.setter
+    def next_node(self, value):
+        if value and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
