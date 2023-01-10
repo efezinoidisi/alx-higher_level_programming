bfce6ec3ccf6df755d7ed5b40a643493f8b22502
efezino idisi <enduranceidisi13@gmail.com>
2023-01-10 03:16:11 +0100

    add task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines the empty BaseGeometry class
+
+"""
+
+
+class BaseGeometry():
+    """
+
+    An empty class that defines the Base Geometry
+
+    """
+    pass
