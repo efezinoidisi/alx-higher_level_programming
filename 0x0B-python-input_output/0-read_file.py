52eb24388cbdbc4575a066df82467af1eb273eae
efezino idisi <enduranceidisi13@gmail.com>
2023-01-10 21:51:01 +0100

    add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module defines the read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to standard output.
+
+    Args:
+        filename (str): name of file to be read
+
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
