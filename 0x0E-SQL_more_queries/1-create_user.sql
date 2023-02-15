4f8725be0e54e9a67f86aea165baa935ab8488ee
efezino idisi <enduranceidisi13@gmail.com>
2023-02-15 19:44:49 +0100

    add task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a user user_0d_1 with all priviledges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
