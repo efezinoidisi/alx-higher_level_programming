55d9af1dcc72765f78d448db338e93790fcc88d7
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2023-02-15 11:29:53 -0800

    fix: change name george from uppercase to capitalize

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,4 +10,4 @@ INSERT INTO second_table(id, name, score) VALUES
        ROW(1, 'John', 10),
        ROW(2, 'Alex', 3),
        ROW(3, 'Bob', 14),
-       ROW(4, 'GEORGE', 8);
+       ROW(4, 'George', 8);
