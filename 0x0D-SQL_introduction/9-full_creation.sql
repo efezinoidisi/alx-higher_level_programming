afbf48d6096ddffa3dcd609b25d53581f9640fe6
efezino idisi <enduranceidisi13@gmail.com>
2023-02-15 09:27:51 +0100

    modify script for task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
 -- creates a table 'second_table' in the database hbtn_0c_0
 
-CREATE TABLE IF NOT EXISTS second_table
-       ('id' INT,
-       'name' VARCHAR(256),
-       'score' INT);
-INSERT INTO second_table('id', 'name', 'score')
-       VALUES ROW(1, 'John', 10),
-       ROW(2, 'Alex', 3),
+CREATE TABLE IF NOT EXISTS second_table ('id' INT,'name' VARCHAR(256),'score' INT);
+INSERT INTO second_table('id', 'name', 'score') VALUES ROW(1, 'John', 10), ROW(2, 'Alex', 3),
        ROW(3, 'Bob', 14),
        ROW(4, 'GEORGE', 8);
