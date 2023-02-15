aeddb90493e55b397383abfda650431de220ddf7
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2023-02-15 11:25:24 -0800

    fix: remove quotes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
--- creates a table 'second_table' in the database hbtn_0c_0
+-- creates a table 'second_table'
+
 
 CREATE TABLE IF NOT EXISTS second_table (
-       'id' INT,
-       'name' VARCHAR(256),
-       'score' INT
+       id INT,
+       name VARCHAR(256),
+       score INT
 );
-INSERT INTO second_table('id', 'name', 'score') VALUES
+INSERT INTO second_table(id, name, score) VALUES
        ROW(1, 'John', 10),
        ROW(2, 'Alex', 3),
        ROW(3, 'Bob', 14),
