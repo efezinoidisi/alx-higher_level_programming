62ac03f20798a000a6f74a8e963e2ef897244150
efezino idisi <enduranceidisi13@gmail.com>
2023-02-22 18:19:53 +0100

    possible fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-# prints numbers from 0 to 99 separated by comma
-
 for i in range(0, 100):
     if i == 99:
         print('{:02d}'.format(i))
-    #else:
-     #   print("{:02d},".format(i), end=" ")
+    else:
+        print("{:02d},".format(i), end=" ")
