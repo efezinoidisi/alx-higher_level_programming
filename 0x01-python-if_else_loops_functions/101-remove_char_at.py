b0d2ea58282778150c8e352c564e482594178bbe
efezino idisi <enduranceidisi13@gmail.com>
2023-02-22 20:42:14 +0100

    add task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    new_str = ""
+    for values in enumerate(str):
+        if values[0] != n:
+            new_str += values[1]
+
+    return new_str
+    
