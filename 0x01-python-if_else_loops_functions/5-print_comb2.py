3aa0ddf137fc6627617a494886b4821a22dc875d
efezino idisi <enduranceidisi13@gmail.com>
2023-02-22 18:16:29 +0100

    debugging

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,7 @@
 
 for i in range(100):
     if i == 99:
-        print("{:02d}".format(i))
+        print(i)
+    #    print("{:02d}".format(i))
     #else:
      #   print("{:02d},".format(i), end=" ")
