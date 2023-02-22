dc9a653f91f3d05144a84c7a079585a59b25d67f
efezino idisi <enduranceidisi13@gmail.com>
2023-02-22 20:44:51 +0100

    fix pycodestyle error

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def remove_char_at(str, n):
     new_str = ""
     for values in enumerate(str):
@@ -7,4 +8,3 @@ def remove_char_at(str, n):
             new_str += values[1]
 
     return new_str
-    
