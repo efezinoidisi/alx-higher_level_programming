3a99b7e81d7844811e77f03562553a62b209a918
efezino idisi <enduranceidisi13@gmail.com>
2023-03-14 17:29:10 +0100

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// This script defines a rectangle with width and height
+// if w or h is equal to zero returns an empty object
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // prints the rectangle using X
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
