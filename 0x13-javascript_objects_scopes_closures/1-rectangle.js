a37ea503e557c87c985cc212242ba19c941593d4
efezino idisi <enduranceidisi13@gmail.com>
2023-03-14 17:10:31 +0100

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// defines a rectangle with width and height
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
