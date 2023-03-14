0da87d7c0f1ce5377d61c11a1475c57777e43597
Endurance Idisi <enduranceidisi13@gmail.com>
2023-03-13 18:07:08 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const sArgs = args.map(arg => parseInt(arg));
 sArgs.sort((a, b) => a - b);
 
 if (sArgs.length === 0 || sArgs.length === 1) {
-  console.log(1);
+  console.log(0);
 } else {
   console.log(sArgs[sArgs.length - 2]);
 }
