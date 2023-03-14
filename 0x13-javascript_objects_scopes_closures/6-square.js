7b61956316c7ed85605b4ad7d542e3fd53a60fbb
efezino idisi <enduranceidisi13@gmail.com>
2023-03-14 18:13:16 +0100

    add comments

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-// This script defines a square that inherits from rectangle
-const S = require('./5-square.js');
+// This script defines a square that inherits from the previous Square
+const PrevSquare = require('./5-square.js');
 
-class Square extends S {
+class Square extends PrevSquare {
+  /* prints the rectangle using the argument passed, if no argument is passed it uses 'X'
+  */
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
