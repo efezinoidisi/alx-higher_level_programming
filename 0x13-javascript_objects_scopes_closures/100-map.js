f5f12da1c5c946f5faa2208d6e724cce9180a3ea
efezino idisi <enduranceidisi13@gmail.com>
2023-03-14 22:23:35 +0100

    task

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// This script imports an array and computes a new array
+
+const list = require('./100-data.js').list;
+const newList = list.map((item, index) => item * index);
+console.log(list);
+console.log(newList);
