6f5ab2f501f0921a24c383d0cdd575a1a2e3f05d
efezino idisi <enduranceidisi13@gmail.com>
2023-03-14 18:56:36 +0100

    refactor

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,12 +5,11 @@ const arg = process.argv[2];
 function factorial (num) {
   if (isNaN(num)) {
     return (1);
-  } else {
-    if (num === 1) {
-      return (1);
-    }
-    return (num * factorial(num - 1));
   }
+  if (num === 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
 }
 
 console.log(factorial(parseInt(arg)));
