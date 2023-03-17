92b4aacffec59f56b4f40e293b882cb6a720e38d
efezino idisi <enduranceidisi13@gmail.com>
2023-03-17 08:10:45 +0100

    task

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained im hbtn_0d_tvshow
+
+SELECT shows.title, genres.genre_id FROM tv_shows AS shows
+       LEFT JOIN tv_show_genres AS genres ON shows.id = genres.show_id
+       ORDER BY shows.title, genres.genre_id;
