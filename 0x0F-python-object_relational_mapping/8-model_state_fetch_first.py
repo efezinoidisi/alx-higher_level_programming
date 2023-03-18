4d2d4b97cf8f6298c79b1236230f6348d4222728
efezino idisi <enduranceidisi13@gmail.com>
2023-03-18 03:54:59 +0100

    print the first State Object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+start link class to table in database
+THis script lists all State objects from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, select, exc
+from sqlalchemy.orm import sessionmaker
+
+
+def query_db(username, password, db):
+    """
+    connect to the database and query database
+
+    Args:
+          username (str): mysql username
+          password (str): mysql password
+          db (str): database name
+    """
+    try:
+        engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost/{db}',
+            pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(engine)
+        session = Session()
+        query = select(State).where(State.id == 1)
+        for state in session.scalars(query):
+            if state:
+                print(state)
+    except exc.SQLAlchemyError as err:
+        print(err)
+    finally:
+        session.close()
+        engine.dispose()
+
+
+if __name__ == "__main__":
+    _, username, password, db = argv
+    query_db(username, password, db)
