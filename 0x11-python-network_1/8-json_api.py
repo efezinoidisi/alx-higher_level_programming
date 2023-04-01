751110031261853630587fadfe62ba5ce4038c60
efezino idisi <enduranceidisi13@gmail.com>
2023-04-01 02:51:17 +0100

    Add script for sending a post request to a URL and displaying the response body

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains script that takes in a URL, sends a post request
+to the URL and displays the body of the response
+"""
+import requests
+from sys import argv
+
+
+def post_url():
+    """
+    sends a post request to a  url using the package requests
+    """
+    payload = {'q': argv[1] if len(argv) > 1 else ""}
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        res = requests.post(url, data=payload)
+        res.raise_for_status()
+        data = res.json()
+        if data:
+            print(f'[{data["id"]}] {data["name"]}')
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError as err:
+        print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    post_url()
