cbd4f094ee24ef6c2820c287db88a5034c97b095
efezino idisi <enduranceidisi13@gmail.com>
2023-04-02 05:41:19 +0100

    refactor code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,26 +5,26 @@ from a github repository. The repo name and owner name are passed
 as command line arguments
 """
 import requests
-import sys
+from sys import argv
 
 
 def get_commits():
     """
-    sends a post request to a  url using the package requests
+    Lists 10 commits from a github repository using the github
+    API by : '<sha>: <author name>'
     """
-    owner = sys.argv[2]
-    repo = sys.argv[1]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    _, repo, owner = argv
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits?per_page=10'
     try:
         res = requests.get(url)
         res.raise_for_status()
         data = res.json()
-        for i in range(10):
-            sha = f"{data[i].get('sha')}: "
-            name = f"{data[i].get('commit').get('author').get('name')}"
+        for item in data:
+            sha = f"{item.get('sha')}: "
+            name = f"{item.get('commit').get('author').get('name')}"
             print(sha + name)
     except requests.exceptions.HTTPError as err:
-        print('None')
+        continue
 
 
 if __name__ == '__main__':
