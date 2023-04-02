ac43eac329dd704ea91b6f0cde4b85fddc9f3de5
efezino idisi <enduranceidisi13@gmail.com>
2023-04-02 04:45:58 +0100

    use github api to access user id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains script that uses the github api to display user id
+"""
+import requests
+from sys import argv
+
+
+def post_url():
+    """
+    sends a post request to a  url using the package requests
+    """
+    headers = {'Authorization': f'token {argv[2]}', 'X-GitHub-Api-Version': "2022-11-28"}
+    url = f'https://api.github.com/users/{argv[1]}'
+    try:
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        data = res.json()
+        print(data.get('id'))
+    except requests.exceptions.HTTPError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    post_url()
