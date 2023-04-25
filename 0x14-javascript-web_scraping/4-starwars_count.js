201c3a94311a06887f017732827670aaf8029763
efezino idisi <enduranceidisi13@gmail.com>
2023-04-25 21:20:09 +0100

    update code logic

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-// displays the title of a Star wars movie based on an id
+// displays the number of movies where the character 'Wedge Antilles' is present
 
 const request = require('request');
 const url = process.argv[2];
-// const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
   const data = JSON.parse(body);
-  const result = data.results[0].characters.filter(e => e.includes('18'))[0];
-  request(result, (error, response, body) => {
-    if (error) console.log(error);
-    const films = JSON.parse(body).films;
-    console.log(films.length);
-  });
+  const result = data.results
+    .filter(data => data.characters
+      .find(character => character.includes('18')));
+  console.log(result.length);
 });
