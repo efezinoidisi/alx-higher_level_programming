01d8e4524baf6150ba99a2086099101edeb2107b
efezino idisi <enduranceidisi13@gmail.com>
2023-04-26 18:05:43 +0100

    fixed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 // displays all characters of a star wars movie
 
-const request = require('request-promise');
+const request = require('request');
+const util = require('util');
+const requestPromise = util.promisify(request)
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 (async function () {
   try {
-    const body = await request(url);
-    const characters = JSON.parse(body).characters;
+    const body = await requestPromise(url);
+    const characters = JSON.parse(body.body).characters;
     for (const character of characters) {
-      const result = await request(character);
-      const name = JSON.parse(result).name;
+      const result = await requestPromise(character);
+      const name = JSON.parse(result.body).name;
       console.log(name);
     }
   } catch (err) {
